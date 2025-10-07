b9f40896040fe50d688597e0fe0f9a324287cfc3
Alexander Vasnev <a-s-vasnev88@yandex.ru>
2025-10-07 13:27:19 +0300

    change getIp

--- backend/src/utils/getIp.ts
+++ backend/src/utils/getIp.ts
@@ -1,13 +1,9 @@
 import {Request} from "express";
-import assert from "node:assert";
 
 export function getIp(req: Request): string | undefined {
-    if (req.ip)
-        return req.ip;
-
-    let ip = req.headers['x-forwarded-for'] ||
-        req.headers['x-real-ip'] ||
-        req.socket.remoteAddress;
+    let ip = req.headers?.['x-forwarded-for']
+        || req.headers?.['x-real-ip']
+        || req.ip;
 
     return Array.isArray(ip) ? ip[0] : ip;
 }
